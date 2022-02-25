74f927051c1e421e919055dfa7ec0a342445dfe3
Isjacrod <israeljacksonrodrigues@gmail.com>
2022-02-25 14:06:18 -0300

    add search form handler

---
+++ js/home.js
@@ -0,0 +1,10 @@
+//Verificação do formulário
+//Simplismente use 'required' no input, estile usando pseudo classe :invalid/:valid/:focus
+/*document.getElementById('search-form').addEventListener('submit', (event) => {
+    var $inputTarget = event.target.querySelector('input[name="q"');
+    if ($inputTarget.value == ''){
+        event.preventDefault();
+        $inputTarget.style.border="1px solid red";
+    }
+}) */
+
