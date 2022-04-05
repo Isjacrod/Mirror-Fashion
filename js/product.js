f664ab41839658d2bde42e8b9c06a5d3eb06a604
Isjacrod <israeljacksonrodrigues@gmail.com>
2022-04-05 16:50:16 -0300

    create function handler to range input

--- js/product.js
+++ js/product.js
@@ -22,3 +22,36 @@ function changeProductImage(ev) {
     let imageId = ev.target.id;
     $productImage.src = productImages[imageId];
 }
+
+
+// Update sizes output
+var availableSizes = [12, 24, 42, 60, 80];
+const $sizeInput = document.getElementById('_size_input');
+const $sizeOutput = document.getElementById('_size_output');
+// Initial Values
+$sizeInput.min = availableSizes[0];
+$sizeInput.max = availableSizes[availableSizes.length - 1];
+$sizeInput.step = 12;
+$sizeOutput.value = $sizeInput.value = availableSizes[parseInt(availableSizes.length / 2)];
+// Update output value
+$sizeInput.oninput = function updateSize() {
+    // Copy array to reverse
+    let temporarySizes = availableSizes.slice();
+    let lastValue = $sizeOutput.value;
+    let newValue = $sizeInput.value;
+    let calbackFunction;
+    if (newValue > lastValue)
+        // Find the size greater than newValue
+        calbackFunction = function(el, findValue=newValue) {return el >= newValue};
+    else if (newValue < lastValue) {
+        // Find the size smaller than newValue
+        temporarySizes.reverse();
+        calbackFunction = function(el, findValue=newValue) {return el <= newValue};
+    // Equal case
+    } else 
+        return
+    outputValue = temporarySizes.find(calbackFunction);
+    $sizeOutput.value = outputValue;
+}
+// Update input value using output value
+$sizeInput.onchange = function() {$sizeInput.value = $sizeOutput.value};
